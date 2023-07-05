4f70a8b9f8d831b47d09ffb5ea2111709af9ecdc
Rahul <rahul.sadhukhan@successive.tech>
2023-07-05 18:28:17 +0530

    WIP

--- components/render-components.tsx
+++ components/render-components.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import Section from './section';
-import HeroBanner from './hero-banner';
-import BlogBanner from './blog-banner';
-import CardSection from './card-section';
-import TeamSection from './team-section';
-import BlogSection from './blog-section';
-import SectionBucket from './section-bucket';
-import AboutSectionBucket from './about-section-bucket';
-import SectionWithHtmlCode from './section-with-html-code';
+// import Section from './section';
+// import HeroBanner from './hero-banner';
+// import BlogBanner from './blog-banner';
+// import CardSection from './card-section';
+// import TeamSection from './team-section';
+// import BlogSection from './blog-section';
+// import SectionBucket from './section-bucket';
+// import AboutSectionBucket from './about-section-bucket';
+// import SectionWithHtmlCode from './section-with-html-code';
 import { RenderProps } from "../typescript/component";
 
 export default function RenderComponents(props: RenderProps) {
@@ -19,69 +19,21 @@ export default function RenderComponents(props: RenderProps) {
       data-contenttype={contentTypeUid}
       data-locale={locale}
     >
-      {pageComponents?.map((component, key: number) => {
-        if (component.hero_banner) {
-          return blogPost ? (
-            <BlogBanner
-              blogBanner={component.hero_banner}
-              key={`component-${key}`}
-            />
-          ) : (
-            <HeroBanner
-              banner={component.hero_banner}
-              key={`component-${key}`}
-            />
-          );
-        }
-        if (component.section) {
-          return (
-            <Section section={component.section} key={`component-${key}`} />
-          );
-        }
-        if (component.section_with_buckets) {
-          return component.section_with_buckets.bucket_tabular ? (
-            <AboutSectionBucket
-              sectionWithBuckets={component.section_with_buckets}
-              key={`component-${key}`}
-            />
-          ) : (
-            <SectionBucket
-              section={component.section_with_buckets}
-              key={`component-${key}`}
-            />
-          );
-        }
-        if (component.from_blog) {
-          return (
-            <BlogSection
-              fromBlog={component.from_blog}
-              key={`component-${key}`}
-            />
-          );
+      {pageComponents?.map((component: any, key: number) => {
+        if (component.hero_section) {
+          return null;
         }
         if (component.section_with_cards) {
-          return (
-            <CardSection
-              cards={component.section_with_cards.cards}
-              key={`component-${key}`}
-            />
-          );
+          return null;
         }
-        if (component.section_with_html_code) {
-          return (
-            <SectionWithHtmlCode
-              embedCode={component.section_with_html_code}
-              key={`component-${key}`}
-            />
-          );
+        if (component.section_with_buckets) {
+          return null;
         }
-        if (component.our_team) {
-          return (
-            <TeamSection
-              ourTeam={component.our_team}
-              key={`component-${key}`}
-            />
-          );
+        if (component.section_with_tables) {
+          return null;
+        }
+        if (component.our_brands) {
+          return null;
         }
       })}
     </div>
