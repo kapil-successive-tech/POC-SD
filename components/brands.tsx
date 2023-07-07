f4e806939380115abc777fa41bd01608078cbbb3
Ankur Singhal <ankur.singhal@ankurss207.successivetech.com>
2023-07-07 13:46:41 +0530

    brands ui  fixes

--- components/brands.tsx
+++ components/brands.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
+import styles from './brands.module.css';
 
 const Brands = ({ brands }: { brands: any }) => {
-    return (<>{brands && <div className='brands'>
+    return (<>{brands && <div className={styles.brands}>
         {brands.map((brand, key) => {
             console.log(brand)
-            return <div className='brand'>
+            return <div className={styles.brand}>
                 <Image  key={`brand-${key}`} alt={brand?.title} src={brand?.brand_image?.url} height={'100%'} width={'100%'} />
             </div>;
         })}
